ac7517248874d5ebcbc94d409b1c40b9dc9f462b
Maryann Henderson <maryannhenderson@Maryanns-MacBook-Air.local>
2022-08-25 11:20:11 +1200

    intial set up sorted

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const server = express()
+
+server.use(express.static(path.join(__dirname, 'public')))
+
+server.get('*', (req, res) => {
+  console.log('server hit!')
+  res.sendFile(path.resolve('server/public/index.html'))
+})
+
+module.exports = server
